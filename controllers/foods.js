48e19e6389f94bbfb225f77f1975eda6d94e87ad
Donovan Campbell <donovan@dlcentertainment.net>
2025-06-22 20:30:13 -0700

    food index route, tested and verified

---
+++ controllers/foods.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user.js');
+
+/*
+Action	Route	HTTP Verb
+Index	‘/users/:userId/foods’	GET
+New	‘/users/:userId/foods/new’	GET
+Create	‘/users/:userId/foods’	POST
+Show	‘/users/:userId/foods/:itemId’	GET
+Edit	‘/users/:userId/foods/:itemId/edit’	GET
+Update	‘/users/:userId/foods/:itemId’	PUT
+Delete	‘/users/:userId/foods/:itemId’	DELETE
+*/
+
+// All routes below are mounted on /users/:userId/foods
+
+// INDEX
+router.get('/', (req, res) => {
+    res.render('foods/index.ejs');
+});
+
+// NEW
+router.get('/new', (req, res) => {
+    res.render('foods/new.ejs');
+})
+
+
+
+module.exports = router;
